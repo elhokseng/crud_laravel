1af6bace9921e253c4e4e2b899974559863a9b0b
Hokseng El <hoksengel3@email.com>
2024-02-21 19:18:23 +0700

    add category to view

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -13,6 +13,7 @@
         <div class=" d-flex justify-content-center">
                 <div class="card border-primary p-4">
                     <h1><span class="text-muted">Name:</span> {{ $product->name }}</h1>
+                    <h3><span class="text-warning">Category :</span> {{ $categorys->name }}</h3>
                     <p><span class="text-muted">Description:</span> {{ $product->description }}</p>
                     <img src="/products/{{ $product->image }}" alt="{{ $product->name }}">
                 </div>
